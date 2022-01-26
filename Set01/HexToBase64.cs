fcf1aa6b3a00417db32fea239fdc63439656007f
Aizenrut <igorce2000@gmail.com>
2022-01-26 19:22:41 -0300

    Hexa para base 64

---
+++ Set01/HexToBase64.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+
+namespace Set01
+{
+    public class HexToBase64
+    {
+        public string Main()
+        {
+            var hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+
+            var bytes = Enumerable.Range(0, hex.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                .ToArray();
+
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
---
+++ Set01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Set01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new HexToBase64();
+            
+            Console.WriteLine(c.Main());
+        }
+    }
+}
